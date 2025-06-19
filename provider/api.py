19e337c6270daa3a85097521c7310dc9163e953e
KirillKornakov <kirill.kornakov@atom.team>
2025-06-19 10:33:55 +0300

    add publish verification_results

--- provider/api.py
+++ provider/api.py
@@ -5,5 +5,5 @@ app = FastAPI()
 @app.get("/users/{user_id}")
 def get_user(user_id: int):
     if user_id == 1:
-        return {"id": 1, "name2": "John Doe"}
+        return {"id": 1, "name": "John Doe"}
     return {"error": "User not found"}, 404
--- provider/test_verify.py
+++ provider/test_verify.py
@@ -1,29 +1,40 @@
 import os
 
 import pytest
-from pact.v3 import Verifier
+from pact import Verifier
 from fastapi.testclient import TestClient
 from api import app
+from dotenv import load_dotenv
+load_dotenv()
 
 @pytest.fixture
 def client():
     return TestClient(app)
 
+
 broker_url = os.getenv('PACT_BROKER_URL', 'http://localhost:9292')
 username = os.getenv('PACT_BROKER_USERNAME', 'username')
 password = os.getenv('PACT_BROKER_PASSWORD', 'password')
+provider_version = os.getenv('GITHUB_REF_NAME', '1.0.10')
+branch = os.getenv('GITHUB_SHA', 'main')
 
 def test_verify_contract(client):
-    verifier = (Verifier("UserServiceProvider")
-                .add_transport(url="http://localhost:8000")
-                .broker_source(url=broker_url, username=username, password=password))
-
+    verifier = Verifier(provider="UserServiceProvider", provider_base_url="http://localhost:8011")
     # Запускаем сервер FastAPI в фоне (можно через subprocess)
     import subprocess
-    proc = subprocess.Popen(["uvicorn", "provider.api:app", "--port", "8000"])
+    proc = subprocess.Popen(["uvicorn", "provider.api:app", "--port", "8011"])
 
-    # Проверяем контракт
-    result = verifier.verify()
+    # Верификация с публикацией результатов
+    result, _= verifier.verify_with_broker(
+        broker_url=broker_url,
+        broker_username=username,
+        broker_password=password,
+        publish_version=provider_version,
+        publish_verification_results=True,
+        provider_version_branch="main",
+        enable_pending=False,
+        verbose=True
+    )
 
     proc.terminate()
-    assert result, f"Pact verification failed"
+    assert result == 0, f"Pact verification failed"
